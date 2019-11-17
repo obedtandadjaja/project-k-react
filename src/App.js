668c70eae4295ae5d376fda4d18aebe15cf299b1
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-11-16 23:53:17 -0800

    make homepage protected route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 
+import RequiresAuth from './higherOrderComponents/requiresAuth'
 import HomePage from './pages/home'
 import './App.css';
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route exact path='/' component={HomePage} />
+        <Route exact path='/' component={RequiresAuth(HomePage)} />
       </BrowserRouter>
     </div>
   );
