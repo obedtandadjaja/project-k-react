37a4fd02d6fdefbc78e3e2e739a139ada6a145e9
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-11-21 12:30:02 -0800

    added property get form

---
+++ src/pages/properties/get.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import Form from './../../components/properties/form'
+import { get } from './../../api/properties'
+
+function PropertyGetPage(props) {
+  const { loading, error, property, get } = props
+
+  useEffect(() => {
+    get(props.match.params.propertyID)
+  }, [])
+
+  return (
+    <div className='propertyGetPage'>
+      <Form initialValues={property} loading={loading} readonly />
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  loading: state.property.getIn(['getLoading']),
+  error: state.property.getIn(['getError']),
+  property: state.property.getIn(['property']),
+})
+const mapDispatchToProps = dispatch => bindActionCreators({
+  get
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(PropertyGetPage)
