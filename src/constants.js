1fa29ef6a0ea301fd6a3ee9963d4c85d5a698b95
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-11-26 19:21:40 -0800

    fix: desktop mode to have the cards not to occupy the whole width

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const AUTH_PROXY_PREFIX = '/auth'
 export const BACKEND_PROXY_PREFIX = '/backend'
 
 export const BACKEND_BASE_URL = process.env.NODE_ENV === 'development' ?
-      `http://api.stage.obedt.com/backend` :
+      `https://api.stage.obedt.com/backend` :
       `https://${API_HOST}${BACKEND_PROXY_PREFIX}`
 
 export const AUTH_BASE_URL = `https://${API_HOST}${AUTH_PROXY_PREFIX}`
