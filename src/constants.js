585e2efbecb507d03ca383362e7a867eb33ddcf8
tralaladudut <tralaladudut@gmail.com>
2020-02-15 21:06:38 +0700

    add color scheme to constant

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,3 @@
-// TODO:(@kenaszogara) need to add color scheme constant
-
 export const API_HOST = process.env.REACT_APP_API_HOST
 export const AUTH_PROXY_PREFIX = '/auth'
 export const BACKEND_PROXY_PREFIX = '/backend'
@@ -10,19 +8,23 @@ export const BACKEND_BASE_URL = process.env.NODE_ENV === 'development' ?
 
 export const AUTH_BASE_URL = `https://${API_HOST}${AUTH_PROXY_PREFIX}`
 
+// (@kenaszogara):
+// https://www.schemecolor.com/modern-pie-chart.php
+// https://www.schemecolor.com/flat-orange-blue-green-pie-chart.php#download
 export const MAINTENANCE_REQUEST_CATEGORY_MAP = new Map([
-      ['electricalAndLightning', { name: 'Electrical and lighting', color: '#D95A6E' }],
-      ['ac', { name: 'AC', color: '#CBA22C' }],
-      ['appliance', { name: 'Appliance', color: '#41AEA8' }],
-      ['doorsAndLocks', { name: 'Doors and locks', color: '#6886BA' }],
-      ['flooring', { name: 'Flooring', color: '#18A0FB' }],
-      ['general', { name: 'General', color: '#1331F4' }],
-      ['plumbingAndBath', { name: 'Plumbing and bath', color: '#2026A2' }],
-      ['preventativeMaintenance', { name: 'Preventative maintenance', color: '#FFA7C9' }],
-      ['pestManagement', { name: 'Pest management', color: '#FFD5C1' }],
-      ['reidentExperience', { name: 'Resident experience', color: '#F4A4A4' }]
+      ['electricalAndLightning', { name: 'Electrical and lighting', color: '#F66D44' }],
+      ['ac', { name: 'AC', color: '#FEAE65' }],
+      ['appliance', { name: 'Appliance', color: '#E6F69D' }],
+      ['doorsAndLocks', { name: 'Doors and locks', color: '#AADEA7' }],
+      ['flooring', { name: 'Flooring', color: '#64C2A6' }],
+      ['general', { name: 'General', color: '#2D87BB' }],
+      ['plumbingAndBath', { name: 'Plumbing and bath', color: '#6050DC' }],
+      ['preventativeMaintenance', { name: 'Preventative maintenance', color: '#D52DB7' }],
+      ['pestManagement', { name: 'Pest management', color: '#FF2E7E' }],
+      ['reidentExperience', { name: 'Resident experience', color: '#4ED1A1' }]
 ])
 
+// https://jsramblings.com/how-to-use-media-queries-with-styled-components/
 export const DEVICE_SIZE = {
       mobileS: `(max-width: 320px)`,
       mobileM: `(max-width: 375px`,
@@ -31,4 +33,19 @@ export const DEVICE_SIZE = {
       laptop: `(max-width: 1024px)`,
       laptopL: `(max-width: 1440px)`,
       desktop: `(max-width: 2560px)`,
+}
+
+// (@kenaszogara): color-scheme from this webpage
+// https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow
+export const COLOR_SCHEME = {
+      darkGrey: '#404040',
+      grey: '#707070',
+      darkBlue: '#091E42',
+      white: '#FAFAFA',
+      whitePale: '#F5F5F5',
+      blue: '#2684FF',
+      lightBlue: '#B3E3FF',
+      green: '#4ED1A1',
+      lightGreen: '#4CD3D6',
+      purple: '#B18BE8'
 }
