5278c2e3c6dcae5b8f56f43e985ec5394f10204c
Zogara K <kenaszogara@live.com>
2020-02-19 11:47:49 +0700

    small clean up

--- src/formHelpers/renderDateField.js
+++ src/formHelpers/renderDateField.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import moment from 'moment'
-import MomentUtils from '@date-io/moment';
+import MomentUtils from '@date-io/moment'
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers'
 
 const renderDateField = ({
